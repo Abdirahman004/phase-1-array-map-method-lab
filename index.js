342c2db4bb5bac921e86a0a85ed2c91d29c3cf93
Abdirahman004 <abdirahman.alio@student.moringaschool.com>
2024-07-14 00:07:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
-
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (title){
+  
+
+    return title.split(" ").map( (word)=> `${word[0] .toUpperCase()}${word.substring(1)}` ).join(" ");
+    
+    
+
+    
+  });
+  
 }
+ //console.log(titleCased());
+
+
+
+ function wordTotitleCase(word){
+  console.log(  `${word[0].toUppercase() }${ word.substring(1)}` )
+
+ }
+ wordTotitleCase("testing");
